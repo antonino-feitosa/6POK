b327e21f546b4b0346e732266fe2ebac20e400f2
antonino-feitosa <antonino_feitosa@yahoo.com.br>
2022-12-31 08:22:51 -0300

    Fix Save/Load Game Data

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
 const fs = require('fs');
 
-const Weltmeister = {
-    save: function(path, json) {
-        let result = { error: 0 };
-        if (path && json && path.length > 0 && json.length > 0) {
-            if (path.endsWith('.js')) {
-                try {
-                    fs.writeFileSync(path, json);
-                } catch (err) {
-                    result = { error: 2, msg: `Couldn't write to file: ${path}` };
-                }
-            } else {
-                result = { error: 3, msg: 'File must have a .js suffix' };
+function saveWeltmeister(path, json) {
+    let result = { error: 0 };
+    if (path && json && path.length > 0 && json.length > 0) {
+        if (path.endsWith('.js')) {
+            try {
+                fs.writeFileSync(path, json);
+            } catch (err) {
+                result = { error: 2, msg: `Couldn't write to file: ${path}` };
             }
         } else {
-            result = { error: 1, msg: 'No Data or Path specified' };
+            result = { error: 3, msg: 'File must have a .js suffix' };
         }
-        return JSON.stringify(result);
+    } else {
+        result = { error: 1, msg: 'No Data or Path specified' };
     }
-};
+    return JSON.stringify(result);
+}
+
+function writeGameData(data){
+    let result = { error: 0 };
+    try {
+        fs.writeFileSync('gamedata.js', data, 'utf8');
+    } catch (err) {
+        result = { error: 1, msg: "Couldn't write data" };
+    }
+    return JSON.stringify(result);
+}
+
+function loadGameData(){
+    let result = { error: 0 };
+    try {
+        if(fs.existsSync(path)){
+            result.data = fs.readFileSync('gamedata.js', 'utf8');
+        } else {
+            result = { error: 2, msg: "Data doesn't exists" };
+        }
+    } catch (err) {
+        result = { error: 1, msg: "Couldn't read data" };
+    }
+    return JSON.stringify(result);
+}
 
 
 const express = require('express');
@@ -30,12 +52,18 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 app.post('/lib/weltmeister/api/save.php', (req, res, next) => {
-    res.send(Weltmeister.save(req.body.path, req.body.data));
+    let result = saveWeltmeister(req.body.path, req.body.data);
+    res.send(result);
 });
 
-app.get('/lib/weltmeister/api/glob.php', (req, res, next) => {
-    console.log(req.body);
-    next();
+app.get('/gamedata.js', (req, res, next) => {
+    let result = loadGameData(req.body.data);
+    res.send(result);
+});
+
+app.post('/gamedata.js', (req, res, next) => {
+    let result = writeGameData(req.body.data);
+    res.send(result);
 });
 
 app.use(sphp.express('./'));
